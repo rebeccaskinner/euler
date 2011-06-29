568652dd2435b63598b881dc578e14eaefab8731
Rebecca Skinner <rebecca@rebeccaskinner.net>
2011-06-28 22:34:48 -0500

    solved project euler problem 3

--- haskell/problem3.hs
+++ haskell/problem3.hs
@@ -1,13 +1,17 @@
 -- The prime factors of 13195 are 5, 7, 13 and 29.
 -- What is the largest prime factor of the number 600851475143 ?
 
---factors n = [ x | x <- [1..n-1], (x `mod` n) == 0]
-factors n = [ x | x <- reverse [1..n-1], odd x, n `mod` x == 0 ]
-empty = (==0) . length
-is_prime n = empty ( filter (/=1) (factors n))
+integer_sqrt = ceiling . sqrt . fromIntegral
+
+is_prime n = let h = integer_sqrt n in
+             [] == [x | x <- [2..h], n `mod` x == 0]
+
+factors n = let h  = integer_sqrt n
+                lf = [x | x <- [2..h], n `mod` x == 0] in
+            (map (div n) lf) ++ (reverse lf)
+
+biggest_prime_factor = head . (filter is_prime) . factors
 
 main = do
-    --let biggestFactor = head (filter is_prime (reverse . factors 600851475143))
-    let facts = reverse $ factors 600851475143
-    let biggestFactor = head ( filter is_prime facts )
-    putStrLn (show biggestFactor)
+    let bpf = biggest_prime_factor 600851475143
+    putStrLn (show bpf)
