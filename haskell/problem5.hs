04fb01af1ebeb26a117e7445624cec64517bcc79
Rebecca Skinner <rebecca@rebeccaskinner.net>
2011-06-29 16:10:29 -0500

    cleaned up problem 5 code

--- haskell/problem5.hs
+++ haskell/problem5.hs
@@ -3,15 +3,10 @@
 -- 
 -- What is the smallest positive number that is evenly divisible by all of the
 -- numbers from 1 to 20?
-import Data.List
-
-intSqrt = ceiling . sqrt . fromIntegral
 isFactor x y = x `mod` y == 0
 stripFactors (x:[]) = [x]
 stripFactors (x:xs) = if or $ map (`isFactor` x) xs
                       then stripFactors xs
                       else x : stripFactors xs
-
 checkDiv x y = and $ map (\n -> y `mod` n == 0) x
-
 main = print $ head [x | x <- [20,40..], checkDiv (stripFactors [1..20]) x]
