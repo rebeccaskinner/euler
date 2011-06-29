9eb2d1732c45b4f00972897e8d676ec7092f6252
Rebecca Skinner <rebecca@rebeccaskinner.net>
2011-06-28 22:44:41 -0500

    cleaned up problem1 code

--- haskell/problem1.hs
+++ haskell/problem1.hs
@@ -3,6 +3,4 @@
 -- 
 -- Find the sum of all the multiples of 3 or 5 below 1000.
 
-main = do
-    let sums = sum [x | x<-[1..999], x `mod` 3 == 0 || x `mod` 5== 0 ]
-    putStrLn (show sums)
+main = print $ sum [x | x<-[1..999], x `mod` 3 == 0 || x `mod` 5== 0 ]
