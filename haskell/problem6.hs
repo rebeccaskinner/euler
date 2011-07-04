c28878fe04ef4a06012c55a4052898bff9f6f7ad
Rebecca Skinner <rebecca@rebeccaskinner.net>
2011-07-04 17:45:12 -0500

    Added second algorithm for problem 6

--- haskell/problem6.hs
+++ haskell/problem6.hs
@@ -16,4 +16,14 @@ sumOfSquares max = sum $ map (^2) [1..max]
 squareOfSum :: Integer -> Integer
 squareOfSum max = sum [1..max] ^ 2
 
-main = print $ squareOfSum 100 - sumOfSquares 100
+-- exploit the fact that (a+b+c+d..)^2 - (a^2+b^2+c^2+d^2) =
+-- a(2b + 2c + 2d) + b(2c + 2d) + c(2d) =
+-- 2ab + 2ac + 2ad + 2bc + 2bd + 2cd
+fastDifference a b = sum $ concatMap (\x -> map (\y -> (2*x*y)) [(x+1)..b]) [a..(b-1)]
+slowDifference n = squareOfSum n - sumOfSquares n
+
+main = do
+    let slow = slowDifference 100
+    let fast = fastDifference 1 100
+    putStrLn ("Fast: " ++ (show fast))
+    putStrLn ("Slow: " ++ (show slow))
